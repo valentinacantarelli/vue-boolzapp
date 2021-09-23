2551e940e6f6213f568e965932949ef44bffb6b8
valentina cantarelli <valentinacanterina@gmail.com>
2021-09-23 15:49:27 +0200

    invio msg

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
-// Milestone 2
-// ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
-// messaggi relativi al contatto attivo all’interno del pannello della conversazione
-// ● Click sul contatto mostra la conversazione del contatto cliccato
+// Milestone 3
+// ● Risposta dell'interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
+// un “ok” come risposta, che apparirà dopo 1 secondo.
 
 
 const app=new Vue(
@@ -106,6 +105,7 @@ const app=new Vue(
                         status: 'sent'
                     });
                 }
+                this.scriviMessaggio="";
             },
         }
     }
